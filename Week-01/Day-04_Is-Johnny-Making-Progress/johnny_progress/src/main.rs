d9c3895ee2d85d426edbbd18c45a5908db88d654
Samuel Alex Marrujo <marrujo.alex@gmail.com>
2026-09-06 18:45:47 -0700

    clean up

--- Week-01/Day-04_Is-Johnny-Making-Progress/johnny_progress/src/main.rs
+++ Week-01/Day-04_Is-Johnny-Making-Progress/johnny_progress/src/main.rs
@@ -1,7 +1,3 @@
-// initial thoughts
-// this is a two-pointer problem
-// track left and check if right > left, then ++ the progress counter
-
 fn progress_checker(runs: Vec<u8>) -> u8 {
     let mut progress_count = 0;
     let mut left = 0;
@@ -16,9 +12,20 @@ fn progress_checker(runs: Vec<u8>) -> u8 {
     progress_count
 }
 
-fn main() {
-    let runs = vec![1,2,3,2];
-    let result = progress_checker(runs);
+/*
+* Examples from project README.md
+*   progressDays([3, 4, 1, 2]) ➞ 2
+*   progressDays([10, 11, 12, 9, 10]) ➞ 3
+*   progressDays([6, 5, 4, 3, 2, 9]) ➞ 1
+*   progressDays([9, 9])  ➞ 0
+*/
 
+fn main() {
+    let runs = vec![3, 4, 1, 2];
+    // let runs = vec![10, 11, 12, 9, 10];
+    // let runs = vec![6, 5, 4, 3, 2, 9];
+    // let runs = vec![9, 9];
+
+    let result = progress_checker(runs);
     println!("{}", result)
 }
